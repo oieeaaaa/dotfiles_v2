25db4b42fd43b37b2a1496e185238b3d96a8c4e2
joe <joimee.cajandab@gmail.com>
2022-10-12 13:07:07 +0800

    bugfix

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -11,40 +11,11 @@ require("nvim-lsp-installer").setup {}
 -- Auto Format
 local null_ls = require("null-ls")
 
-null_ls.setup({
-  on_attach = function(client, bufnr)
-    if client.server_capabilities.documentFormattingProvider then
-      vim.cmd("nnoremap <silent><buffer> <Leader>p :lua vim.lsp.buf.formatting()<CR>")
-
-      -- format on save
-      vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
-    end
-
-    if client.server_capabilities.documentRangeFormattingProvider then
-      vim.cmd("xnoremap <silent><buffer> <Leader>p :lua vim.lsp.buf.range_formatting({})<CR>")
-    end
-  end,
-})
+null_ls.setup {}
 
 local prettier = require("prettier")
 
-prettier.setup({
-  bin = 'prettier', -- or `'prettierd'` (v0.22+)
-  filetypes = {
-    "css",
-    "graphql",
-    "html",
-    "javascript",
-    "javascriptreact",
-    "json",
-    "less",
-    "markdown",
-    "scss",
-    "typescript",
-    "typescriptreact",
-    "yaml",
-  },
-})
+prettier.setup {}
 
 -- Servers
 require'lspconfig'.pyright.setup{}
